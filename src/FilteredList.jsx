a6276f9dbc6f8da6a1dfd81bd47808296620cf9e
IzzyFelix <iz4751@hotmail.com>
2025-04-20 21:53:57 -0400

    updated filtered list

--- src/FilteredList.jsx
+++ src/FilteredList.jsx
@@ -1,60 +1,43 @@
 import React, { Component } from 'react';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
 import List from './List';
 
 class FilteredList extends Component {
   constructor(props) {
     super(props);
 
-    // Add "type" to the state to store the filter category (Fruit, Vegetables, or All)
     this.state = {
       search: "",
-      type: "All"  // Default filter value
+      type: "All" // Add default type state
     };
   }
 
-  // Sets the search state whenever the user types in the search bar
   onSearch = (event) => {
     this.setState({ search: event.target.value.trim().toLowerCase() });
   };
 
-  // Sets the "type" state variable depending on the selected filter from the dropdown
-  onFilter = (selectedType) => {
-    this.setState({ type: selectedType });
+  onFilter = (event) => {
+    this.setState({ type: event.target.text });
   };
 
-  // Filters items based on the search term and selected type
   filterItem = (item) => {
     const { search, type } = this.state;
-    const matchesSearch = item.name.toLowerCase().includes(search);
-    const matchesType = type === "All" || item.type === type;
-
-    return matchesSearch && matchesType;
+    return (
+      item.name.toLowerCase().includes(search) &&
+      (type === "All" || item.type === type)
+    );
   };
 
   render() {
     return (
       <div className="filter-list">
-        
-        {/* Dropdown Button to filter by Fruit, Vegetables, or All */}
-        <DropdownButton
-          title={this.state.type}
-          id="filter-dropdown"
-          onSelect={this.onFilter}
-        >
-          <MenuItem eventKey="Fruit">Fruit</MenuItem>
-          <MenuItem eventKey="Vegetable">Vegetable</MenuItem>
-          <MenuItem eventKey="All">All</MenuItem>
+        <DropdownButton title="Select Type" id="filter-dropdown">
+          <Dropdown.Item eventKey="1">Fruit</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Vegetable</Dropdown.Item>
+          <Dropdown.Item eventKey="3">All</Dropdown.Item>
         </DropdownButton>
 
-        {/* Search input */}
-        <input
-          type="text"
-          placeholder="Search"
-          onChange={this.onSearch}
-        />
-
-        {/* Pass the filtered items to the List component */}
+        <input type="text" placeholder="Search" onChange={this.onSearch} />
         <List items={this.props.items.filter(this.filterItem)} />
       </div>
     );
